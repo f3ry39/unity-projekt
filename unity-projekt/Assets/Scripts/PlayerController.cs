48346d3e675b30d4aab6f8f639213974cc73220a
Bartosz Gryciuk <f3ry39@gmail.com>
2022-10-25 17:03:41 +0200

    poruszanie sie + animacja skoku

--- unity-projekt/Assets/Scripts/PlayerController.cs
+++ unity-projekt/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public float heroSpeed;
     Animator anim;
     bool dirToRight = true; 
+    Rigidbody2D rgdBody;
+
+    public float jumpForce;
+
+    private bool onTheGround;
+    public Transform groundTester;
+    private float radius = 0.1f;
+    public LayerMask layersToMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent <Animator> ();
+        rgdBody = GetComponent <Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update()
     {
+        onTheGround = Physics2D.OverlapCircle(groundTester.position, radius, layersToMask);
 
         float horizontalMove = Input.GetAxis ("Horizontal");
+        rgdBody.velocity = new Vector2 (horizontalMove * heroSpeed, rgdBody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && onTheGround)
+        { 
+            rgdBody.AddForce(new Vector2(0f, jumpForce));
+            anim.SetTrigger("jump");
+        }
 
         anim.SetFloat("speed", Mathf.Abs (horizontalMove)); //przejscie z animacji na druga animacje
 
