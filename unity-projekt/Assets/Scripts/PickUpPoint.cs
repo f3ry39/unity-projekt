f1eb5dcbfd28af757e3aea17febf174081d669a6
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-10 15:03:14 +0100

    zbieranie punktów

---
+++ unity-projekt/Assets/Scripts/PickUpPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
