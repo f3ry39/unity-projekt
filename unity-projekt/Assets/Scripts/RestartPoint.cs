6590110b1d3989c05292d415f16a4faf6e6867e4
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-11 20:08:32 +0100

    Kolorowanie zdobytych checkpoint'ów

--- unity-projekt/Assets/Scripts/RestartPoint.cs
+++ unity-projekt/Assets/Scripts/RestartPoint.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class RestartPoint : MonoBehaviour
 {
     RestartPointsManager restartPointsManager;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         restartPointsManager = GameObject.Find("Manager").GetComponent<RestartPointsManager>();
+
         if(restartPointsManager == null)
         {
             Debug.LogError("RestartPointsManager nie zosta³ znaleziony");
         }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -21,6 +26,7 @@ public class RestartPoint : MonoBehaviour
         if  (other.gameObject.tag == "Player")
         {
             restartPointsManager.UpdateStartPoint(this.gameObject.transform);
+            spriteRenderer.color = new Color(0.3f, 0.6f, 0.6f);
         }
     }
 }
