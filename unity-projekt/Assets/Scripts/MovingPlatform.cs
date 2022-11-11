2dc5755f79709a111107b490f85680cbdddb2674
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-11 22:11:59 +0100

    naprawa błędów platformy

--- unity-projekt/Assets/Scripts/MovingPlatform.cs
+++ unity-projekt/Assets/Scripts/MovingPlatform.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Vector2 startPoint;
-    public Vector2 endPoint;
+    public Transform navigationStartPoint;
+    public Transform navigationEndPoint;
+
+    private Vector2 startPoint;
+    private Vector2 endPoint;
     public float speed;
 
     private Vector2 currentPlatformPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPoint = navigationStartPoint.position;
+        endPoint = navigationEndPoint.position;
+        Destroy(navigationStartPoint.gameObject);
+        Destroy(navigationEndPoint.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentPlatformPosition = Vector2.Lerp(startPoint, endPoint, Mathf.PingPong(Time.time, 1));
+        currentPlatformPosition = Vector2.Lerp(startPoint, endPoint, Mathf.PingPong(Time.time * speed, 1));
         transform.position = currentPlatformPosition;
-        //transform.Translate(speed, 0, 0);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.gameObject.name);
-        other.transform.parent = transform;
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.parent = transform;
+            other.attachedRigidbody.Sleep();
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log(other.gameObject.name + " - OUT");
-        other.transform.parent = null;
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
     }
 }
